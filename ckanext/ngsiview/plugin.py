8d5e92db638b2f6ec418d2bf8b8d0f9495e518a5
gzarrub <g.zarrub@gmail.com>
2015-03-30 14:44:21 +0200

    test

--- ckanext/ngsiview/plugin.py
+++ ckanext/ngsiview/plugin.py
@@ -3,42 +3,42 @@ import logging
 from ckan.common import json
 import ckan.plugins as p
 import ckanext.resourceproxy.plugin as proxy
+from ckan.plugins import toolkit
 import ckan.lib.datapreview as datapreview
+from ckan.common import _, request
 
 log = logging.getLogger(__name__)
 
-DEFAULT_NGSI_FORMATS = ['ngsi9', 'ngsi10']
-
-
-def get_formats(config):
-
-    out = {}
-
-    ngsi_formats = config.get('ckan.preview.ngsi_formats', '').split()
-    out['ngsi_formats'] = ngsi_formats or DEFAULT_NGSI_FORMATS
-
-    return out
-
 
 class TextView(p.SingletonPlugin):
-    '''This extension previews JSON(P).'''
+
+    NGSI_FORMATS = ['ngsi9', 'ngsi10']
 
     p.implements(p.IConfigurer, inherit=True)
     p.implements(p.IConfigurable, inherit=True)
     p.implements(p.IResourceView, inherit=True)
 
     proxy_is_enabled = False
-    ngsi_formats = []
+    oauth2_is_enabled = False
+
 
     def update_config(self, config):
 
-        formats = get_formats(config)
-        for key, value in formats.iteritems():
-            setattr(self, key, value)
-
+        p.implements(p.IRoutes, inherit=True)
         p.toolkit.add_public_directory(config, 'theme/public')
         p.toolkit.add_template_directory(config, 'theme/templates')
         p.toolkit.add_resource('theme/public', 'ckanext-textview')
+
+    def before_map(self, m):
+        m.connect('/dataset/{id}/resource/{resource_id}/ngsiproxy',
+                  controller='ckanext.ngsipreview.controller:ProxyNGSIController', action='proxy_ngsi_resource')
+        return m
+
+    def get_proxified_ngsi_url(self, data_dict):
+        url = h.url_for(action='proxy_ngsi_resource', controller='ckanext.ngsipreview.controller:ProxyNGSIController',
+                        id=data_dict['package']['name'], resource_id=data_dict['resource']['id'])
+        log.info('NGSI proxified url is {0}'.format(url))
+        return url
 
     def info(self):
         return {'name': 'ngsi_view',
@@ -47,34 +47,59 @@ class TextView(p.SingletonPlugin):
                 'default_title': p.toolkit._('NGSI'),
                 }
 
+    def check_query(self, resource):
+        if (resource['url'].lower().find('/querycontext') != -1
+                or resource['url'].lower().find('/contextentities/') != -1):
+            return True
+        else:
+            return False
+
     def can_view(self, data_dict):
         resource = data_dict['resource']
+        if 'oauth_req' not in resource:
+            oauth_req = 'false'
+        else:
+            oauth_req = resource['oauth_req']
+
+
         format_lower = resource.get('format', '').lower()
+        pattern = "/dataset/"+data_dict['package']['name']+"/resource/"
+
         proxy_enabled = p.plugin_loaded('resource_proxy')
-        same_domain = datapreview.on_same_domain(data_dict)
-        if format_lower in self.jsonp_formats:
-            return True
-        if format_lower in self.no_jsonp_formats:
-            return proxy_enabled or same_domain
-        return False
+
+        if format_lower in self.NGSI_FORMATS:
+            same_domain = datapreview.on_same_domain(data_dict)
+            pattern = "/dataset/"+data_dict['package']['name']+"/resource/"
+            if same_domain or proxy_enabled:
+                if self.check_query(resource) and request.path.find(pattern) != -1 and oauth_req == 'true' and not toolkit.c.user:
+                    return False
+                elif self.check_query(resource) and request.path.find(pattern) != -1 and oauth_req == 'false' and not self.oauth2_is_enabled:
+                   return False
+                elif (resource['url'].lower().find('/querycontext') != -1
+                      and request.path.find(pattern) != -1 and 'payload' not in resource):
+                    return False
+                else:
+                    return True
+            else:
+                return False
+        else:
+            return False
 
     def setup_template_variables(self, context, data_dict):
-        metadata = {'text_formats': self.text_formats,
-                    'json_formats': self.json_formats,
-                    'jsonp_formats': self.jsonp_formats,
-                    'xml_formats': self.xml_formats}
+        metadata = {'ngsi_formats': self.ngsi_formats}
 
-        url = proxy.get_proxified_resource_url(data_dict)
-        format_lower = data_dict['resource']['format'].lower()
-        if format_lower in self.jsonp_formats:
-            url = data_dict['resource']['url']
+        if not datapreview.on_same_domain(data_dict):
+            if self.check_query(data_dict['resource']):
+                url = self.get_proxified_ngsi_url(data_dict)
+            else:
+                url = proxy.get_proxified_resource_url(data_dict)
 
-        return {'preview_metadata': json.dumps(metadata),
-                'resource_json': json.dumps(data_dict['resource']),
-                'resource_url': json.dumps(url)}
+            return {'preview_metadata': json.dumps(metadata),
+                    'resource_json': json.dumps(data_dict['resource']),
+                    'resource_url': json.dumps(url)}
 
     def view_template(self, context, data_dict):
-        return 'text_view.html'
+        return 'ngsi.html'
 
     def form_template(self, context, data_dict):
         return 'text_form.html'
