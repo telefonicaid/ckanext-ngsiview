fdd38fef76ceb444c8221d7dcf00813220615af4
gzarrub <g.zarrub@gmail.com>
2015-04-07 11:47:45 +0200

    headers changed and license files added

--- ckanext/ngsiview/plugin.py
+++ ckanext/ngsiview/plugin.py
@@ -1,3 +1,21 @@
+#!/usr/bin/env python
+# Copyright 2015 Telefonica Investigacion y Desarrollo, S.A.U
+#
+# This file is part of ckanext-ngsipreview.
+#
+# Ckanext-ngsiview is free software: you can redistribute it and/or
+# modify it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# Ckanext-ngsiview is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero
+# General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with Orion Context Broker. If not, see http://www.gnu.org/licenses/.
+
 import logging
 
 from ckan.common import json
--- setup.py
+++ setup.py
@@ -1,9 +1,29 @@
-from setuptools import setup, find_packages  # Always prefer setuptools over distutils
+#!/usr/bin/env python
+# Copyright 2015 Telefonica Investigacion y Desarrollo, S.A.U
+#
+# This file is part of ckanext-ngsipreview.
+#
+# Ckanext-ngsiview is free software: you can redistribute it and/or
+# modify it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# Ckanext-ngsiview is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero
+# General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with Orion Context Broker. If not, see http://www.gnu.org/licenses/.
+
+from setuptools import setup, find_packages
 
 setup(
     name='ckanext-ngsiview',
-    version='0.0.1',
+    version='0.1',
     url='https://github.com//ckanext-ngsiview',
+    author='Guillermo Zarzuelo',
+    author_email='gzarrub@gmail.com',
     classifiers=[
         'Development Status :: 4 - Beta',
         'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
