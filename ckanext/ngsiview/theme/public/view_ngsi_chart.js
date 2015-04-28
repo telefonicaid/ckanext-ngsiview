38f204bdeb77e7180c7a5a90c3964d2f6f1e9cd9
gzarrub <g.zarrub@gmail.com>
2015-04-28 12:33:36 +0200

    first attempt to show a chart

---
+++ ckanext/ngsiview/theme/public/view_ngsi_chart.js
@@ -0,0 +1,25 @@
+ckan.module('ngsiviewchart',function(jQuery,_){
+    $('#chart').highcharts({
+        chart: {
+            type: 'spline'
+        },
+        xAxis: {
+            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+        },
+        plotOptions: {
+            series: {
+                allowPointSelect: true
+            }
+        },
+        series: [
+        {
+            name: 'attribute1',
+            data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
+        },
+        {
+            name: 'attribute2',
+            data: [71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4, 20.1]
+        }
+        ]
+    });
+});
