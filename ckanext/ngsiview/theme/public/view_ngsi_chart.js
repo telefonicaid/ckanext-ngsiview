7d777a8791cbb69f1e145a300ac22b2fa7b0df98
gzarrub <g.zarrub@gmail.com>
2015-05-18 14:12:20 +0200

    added Dygraph as graphics library

--- ckanext/ngsiview/theme/public/view_ngsi_chart.js
+++ ckanext/ngsiview/theme/public/view_ngsi_chart.js
@@ -6,73 +6,98 @@ ckan.module('ngsiviewchart',function(jQuery,_){
                             dataType:'json',
                             dataConverter:function(data){return JSON.stringify(data,null,2);},
                             language:'json',type:'GET'}},
-            initialize:function(){
-                var self=this;
-                var p;
-                p=this.options.parameters;
-                if(typeof(view_enable) == 'undefined'){
-                    view_enable = [];
-                    view_enable[0] = true;
-                    resource_url = preload_resource['url']
-                }
-                if(view_enable[0]){
-                    jQuery.ajax(resource_url,{
-                        type:p.type,
-                        contentType:p.contentType,
-                        dataType:p.dataType,
-                        success:function(data,textStatus,jqXHR){
-                            if(preload_resource['format'].toLowerCase() == 'ngsi-sth'){
-                                document.getElementById('chart').style.height = '400px';
-                                document.getElementById('chart').style.border = '1px solid rgba(0, 0, 0, 0.15)';
+        initialize:function(){
+            var self=this;
+            var p;
+            p=this.options.parameters;
 
-                                var series = [];
-                                createChart = function (series) {
-                                    $('#chart').highcharts('StockChart', {
-                                            title: {text: ''},
-                                            series: series
-                                        });
-                                }
+            if(typeof(view_enable) == 'undefined'){
+                view_enable = [];
+                view_enable[0] = true;
+                resource_url = preload_resource['url']
+            }
 
-                                if(typeof(data.contextResponses) != 'undefined'){
-                                    var entity_id = '';
-                                    var attribute_name = '';
-                                    var attributes = [];
+	    var DygraphContent = "";
+	    function makeGraph(DygraphContent){
+     		new Dygraph(
+          		document.getElementById("chart"),
+          		DygraphContent,
+          		{
+           		 fillGraph: 'true',
+           		 rollPeriod: 14,
+           		 legend: 'always',
+           		 labelsDivStyles: { 'textAlign': 'right' },
+           		 showRangeSelector: true
+          		}
+      		);
+	    }
 
-                                    for(i=0;i<data.contextResponses.length;i++){
-                                        entity_id = data.contextResponses[i].contextElement.id;
-                                        attributes = data.contextResponses[i].contextElement.attributes;
-                                        for(e=0;e<attributes.length;e++){
-                                            var v_list = [];
-                                            attribute_name = attributes[e].name;
-                                            for(z=0;z<attributes[e].values.length;z++){
-                                                date = Date.parse(attributes[e].values[z].recvTime);
-                                                value = parseFloat(attributes[e].values[z].attrValue);
-                                                v_list[v_list.length]=[date, value];
+            if(view_enable[0]){
+                jQuery.ajax(resource_url,{
+                    type:p.type,
+                    contentType:p.contentType,
+                    dataType:p.dataType,
+                    success:function(data,textStatus,jqXHR){
+                        if(preload_resource['format'].toLowerCase() == 'ngsi-sth'){
+                            document.getElementById('chart').style.height = '400px';
+                            document.getElementById('chart').style.border = '1px solid rgba(0, 0, 0, 0.15)';
+
+                            if(typeof(data.contextResponses) != 'undefined'){
+                                var entity_id = '';
+                                var attribute_name = '';
+                                var attributes = [];
+				var v_list = [];
+				var d_list = [];
+                                for(i=0;i<data.contextResponses.length;i++){
+                                    attributes = data.contextResponses[i].contextElement.attributes;
+                                    for(e=0;e<attributes.length;e++){
+					var i_a = data.contextResponses[i].contextElement.id+"_"+attributes[e].name;
+					v_list[v_list.length] = ['Date', i_a]; 
+					d_list[d_list.length] = ['Date', i_a];
+                                        for(z=0;z<attributes[e].values.length;z++){
+					    var d = attributes[e].values[z].recvTime.substring(0,10).replace(/-/gi, "/");
+					    var h = attributes[e].values[z].recvTime.substring(11,19)
+				            var g_date = d+" "+h;
+					    var value  = parseFloat(attributes[e].values[z].attrValue);					    
+                                            v_list[v_list.length] = [g_date, value];
+					    d_list[d_list.length] = [attributes[e].values[z].recvTime, value];
+		                        }
+        	                            var csvContent = "";
+	                                    var DygraphContent = "";
+
+                                            for(var z=0;z<v_list.length;z++){
+                                                DygraphContent += v_list[z].join(", ")+"\n";
+                                                csvContent += d_list[z].join(", ")+"%0A";
                                             }
-                                            series[series.length]={
-                                                name: entity_id+"-"+attribute_name,
-                                                type: 'area',
-                                                gapSize: 5,
-                                                tooltip: {
-                                                    valueDecimals: 2
-                                                },
-                                                fillColor : {
-                                                    linearGradient : { x1: 0, y1: 0, x2: 0, y2: 1},
-                                                    stops : [
-                                                        [0, Highcharts.getOptions().colors[e]],
-                                                        [1, Highcharts.Color(Highcharts.getOptions().colors[e]).setOpacity(0).get('rgba')]
-                                                    ]
-                                                },
-                                                threshold: null,
-                                                data: v_list,
-                                            }
-                                        }
-                                    }
-                                    createChart(series);
-                                }
+				            makeGraph(DygraphContent);
+                                     }
+				}
+
+			        var dbtn ="<a id='dbtn' class='btn btn-primary'><b>Download</b></a>";
+                                $("#chart").before(dbtn);
+				document.getElementById('dbtn').addEventListener('click', function () {
+
+					var a         = document.createElement('a');
+					a.href        = 'data:attachment/csv,' + csvContent;
+					a.target      = '_blank';
+					a.download    = i_a+'.csv';
+
+					document.body.appendChild(a);
+					a.click();
+				}, false);
                             }
                         }
-                    });
-                }
+                    },
+                    error:function(jqXHR,textStatus,errorThrown){
+                        if(textStatus=='error'&&jqXHR.responseText.length){
+                            document.getElementById('chart').style.height = '0px';
+                            document.getElementById('chart').style.border = '0px';
+                        }
+                        else{document.getElementById('chart').style.height = '0px';;
+                            document.getElementById('chart').style.border = '0px';}}
+                });
             }
-    }});
+        }
+    }
+});
+
